9224106460e7d2cc2dad3b5e9e383ec574583ac5
Hedmon0094 <123468785+Hedmon0094@users.noreply.github.com>
2024-02-12 12:07:23 +0300

    fishfeast

---
+++ reg.php
@@ -0,0 +1,13 @@
+<?php 
+$servername="localhost";
+$username="root";
+$password="";
+$db_name="register";
+$conn=new mysqli($servername,$username,$password,$db_name,3306);
+if($conn->connect_error){
+    die("Connection failed".
+    $conn->connect_error);
+}
+echo("Connection ok");
+
+
